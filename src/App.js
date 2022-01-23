084a04462e5b3511b96b14f236c7e30409fe27af
avi-11 <avi1109cool@gmail.com>
2022-01-23 13:35:58 +0530

    --done--

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import Home from "./components/Home";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Questions from "./components/Questions";
 import Result from "./components/Result";
 
